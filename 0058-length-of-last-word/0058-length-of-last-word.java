5b889fdd1b2158e56f0e81d8713cfa4d1f133760
Sai Kumar Vaddepally <42552139+saikumarvaddepally15@users.noreply.github.com>
2024-08-09 12:17:47 -0400

    Time: 1 ms (35.14%), Space: 41.7 MB (28.57%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String words[]=s.split(" ");
+        String lastword= words[words.length-1];
+        return lastword.length();
+    }
+}
