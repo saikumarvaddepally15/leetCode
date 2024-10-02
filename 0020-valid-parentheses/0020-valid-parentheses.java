c98fd4ce4ac667233674d57fd6106720844734fd
Sai Kumar Vaddepally <42552139+saikumarvaddepally15@users.noreply.github.com>
2024-10-02 13:06:48 -0400

    Time: 2 ms (52.71%), Space: 41.7 MB (24.82%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st= new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+                st.add(s.charAt(i));
+            }else{
+                if(st.isEmpty()) return false;
+                char top=st.peek();
+                if((s.charAt(i)==')' && top=='(') || (s.charAt(i)==']' && top=='[') || (s.charAt(i)=='}' && top=='{') ) {
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
